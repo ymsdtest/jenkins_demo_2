bc2b61641380817e3afbc08aa9a58d370efcb736
ymsdtest <50386518+ymsdtest@users.noreply.github.com>
2019-06-04 17:46:34 +0800

    增加下单和支付测试用例

--- case/test_web.py
+++ case/test_web.py
@@ -13,6 +13,16 @@ class TestWeb(unittest.TestCase):
         """web端用户注册"""
         print("输入web用户名:\n输入电话\n输入邮箱")
         self.assertEqual(2,2)
+    
+    def test_order_web(self):
+        """web端下单"""
+        print("选择商品:\n选择数量\n选择样式")
+        self.assertEqual(2,3)
+        
+    def test_pay_web(self):
+        """web端支付"""
+        print("消费金额")
+        self.assertEqual(2,2)
 
     def tearDown(self):
         print("关闭浏览器")
