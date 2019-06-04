41b519d720db80b90585d1fc0587bac91ffd68e0
ymsdtest <50386518+ymsdtest@users.noreply.github.com>
2019-06-04 13:14:17 +0800

    Update test_app.py
    增加下单测试用例

--- case/test_app.py
+++ case/test_app.py
@@ -14,6 +14,11 @@ class TestWeb(unittest.TestCase):
         """app端用户注册"""
         print("输入app用户名:\n输入电话\n输入邮箱")
         self.assertEqual(2, 2)
+        
+    def test_order_app(self):
+        """app端用户下单"""
+        print("购买商品:\n输入数量\n输入金额")
+        self.assertEqual(2, 2)
 
     def tearDown(self):
         print("关闭浏览器")
